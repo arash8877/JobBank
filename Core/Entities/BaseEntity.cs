9fa5b9b2d11d0d959fff010f7215d27daae77517
Arash <arash8877@gmail.com>
2025-09-24 16:51:00 +0200

    create base entitity

---
+++ Core/Entities/BaseEntity.cs
@@ -0,0 +1,16 @@
+
+
+namespace Core.Entities
+{
+
+    public class BaseEntity
+    {
+        [Key]
+        public long Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        public bool IsActive { get; set; }
+        
+    }
+        
+}
