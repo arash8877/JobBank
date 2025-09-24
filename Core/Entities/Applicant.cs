3e49f034b889d9f703249899e1a4b2e967b94637
Arash <arash8877@gmail.com>
2025-09-24 20:53:33 +0200

    create entity for Applicant

--- Core/Entities/Applicant.cs
+++ Core/Entities/Applicant.cs
@@ -3,11 +3,19 @@
 namespace Core.Entities
 {
 
-    public class Applicant: BaseEntity
+    public class Applicant : BaseEntity
     {
-      
-      
-        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string CoverLetter { get; set; }
+        public string ResumeUrl { get; set; }
+        public string ResumeUrl { get; set; }
+
+        // Relations
+        public long JobId { get; set; }
+        public Job Job { get; set; }
     }
         
 }
